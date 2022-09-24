f8e26e98ee50147ccfaeeb6833ea7953f437c865
Nam Bui <nam.bui17@icloud.com>
2022-09-24 16:26:40 -0500

    remove debugger

--- assets/js/script.js
+++ assets/js/script.js
@@ -212,7 +212,6 @@ replay.addEventListener("click", function(event) {
 
 endSub.addEventListener("click", function(event) {
     event.preventDefault();
-    debugger
     console.log(highScore.initials);
     if (score >= highScore.high) {
         highScore.initials = endIni.value;
